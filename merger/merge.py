ee594c9c9b01e3c75f3e2cff617094d035af6b19
Dirk Gómez <dirk@dirkgomez.de>
2018-10-08 08:48:19 +0200

    Add html minifier

--- merger/merge.py
+++ merger/merge.py
@@ -2,6 +2,7 @@ import json
 from jinja2 import Template
 import pprint
 import datetime
+import htmlmin
 
 #import code; code.interact(local=dict(globals(), **locals()))
 
@@ -17,7 +18,11 @@ def merge_data():
 
     with open("/templates/index.template", "r") as template_file:
         events_template = Template(template_file.read().strip())
-        print(events_template.render(events = sorted_events, now = datetime.datetime.now()))
+        print(htmlmin.minify(
+            events_template.render(events = sorted_events, now = datetime.datetime.now()),
+            remove_comments=True, remove_empty_space=True
+            )
+            )
 
 if __name__ == "__main__":
     merge_data()
