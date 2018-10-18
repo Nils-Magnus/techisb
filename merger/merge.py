836d417c4b1f8f375699b535c60a5c1f7fc2086d
Dirk Gómez <dirk@dirkgomez.de>
2018-10-18 20:15:15 +0200

    Add 'add to calendar' link for mobile view

--- merger/merge.py
+++ merger/merge.py
@@ -7,11 +7,49 @@ import sys
 import itertools
 import pytz
 import glob
+import copy
 
 
 def merge_data(web_dir, json_dir):
     def _to_datetime(date, time):
         return datetime.datetime.strptime(date + ' ' + time, '%Y-%m-%d %H:%M')
+
+    def _calendar_header():
+        # generate ical file
+        calendar = icalendar.Calendar({
+                'PRODID': '-//TechisBe//Berlin tech events//DE',
+                'METHOD': 'PUBLISH',
+                'VERSION': '2.0',
+                'X-WR-CALNAME': 'TechisB - Berlin tech events via http://techisb.de',
+                'X-WR-TIMEZONE': 'Europe/Berlin',
+                'X-WR-CALDESC': 'All the relevant Berlin tech events in one calendar'
+                })
+
+        # Provide timezone info -
+        # see https://github.com/collective/icalendar/blob/master/src/icalendar/tests/test_timezoned.py#L50
+        tzc = icalendar.Timezone()
+        tzc.add('tzid', 'Europe/Berlin')
+        tzc.add('x-lic-location', 'Europe/Berlin')
+
+        tzs = icalendar.TimezoneStandard()
+        tzs.add('tzname', 'CET')
+        tzs.add('dtstart', datetime.datetime(1970, 10, 25, 3, 0, 0))
+        tzs.add('rrule', {'freq': 'yearly', 'bymonth': 10, 'byday': '-1su'})
+        tzs.add('TZOFFSETFROM', datetime.timedelta(hours=2))
+        tzs.add('TZOFFSETTO', datetime.timedelta(hours=1))
+
+        tzd = icalendar.TimezoneDaylight()
+        tzd.add('tzname', 'CEST')
+        tzd.add('dtstart', datetime.datetime(1970, 3, 29, 2, 0, 0))
+        tzs.add('rrule', {'freq': 'yearly', 'bymonth': 3, 'byday': '-1su'})
+        tzd.add('TZOFFSETFROM', datetime.timedelta(hours=1))
+        tzd.add('TZOFFSETTO', datetime.timedelta(hours=2))
+
+        tzc.add_component(tzs)
+        tzc.add_component(tzd)
+        calendar.add_component(tzc)
+
+        return calendar
 
     now = datetime.datetime.now()
 
@@ -22,10 +60,16 @@ def merge_data(web_dir, json_dir):
             events = events + json.loads(file.read())
 
     # sort them and provide two iterators for html and ical generation
-    html_data, mobile_data, ics_data = itertools.tee(filter(
+    """
+    sorted_events = filter(
             lambda x: _to_datetime(x['date'], x['time']) > now,
-            sorted(events, key=lambda x: x['date'] + x['time']),
-            ), 3)
+            sorted(events, key=lambda x: x['date'] + x['time']))
+    """
+    future_events = filter(lambda x: _to_datetime(x['date'], x['time']) > now, events)
+    sorted_events = sorted(future_events, key=lambda x: x['date'] + x['time'])
+
+    html_data, mobile_data, ics_data = itertools.tee(
+            [dict(item, eventnumber=sorted_events.index(item)) for item in sorted_events], 3)
 
     # generate html file
     with open('templates/index.template', 'r') as template_file, open(web_dir + 'index.html', 'w') as output_file:
@@ -45,43 +89,11 @@ def merge_data(web_dir, json_dir):
             )
         )
 
-    # generate ical file
-    calendar = icalendar.Calendar({
-            'PRODID': '-//TechisBe//Berlin tech events//DE',
-            'METHOD': 'PUBLISH',
-            'VERSION': '2.0',
-            'X-WR-CALNAME': 'TechisB - Berlin tech events via http://techisb.de',
-            'X-WR-TIMEZONE': 'Europe/Berlin',
-            'X-WR-CALDESC': 'All the relevant Berlin tech events in one calendar'
-            })
-
-    # Provide timezone info -
-    # see https://github.com/collective/icalendar/blob/master/src/icalendar/tests/test_timezoned.py#L50
-    tzc = icalendar.Timezone()
-    tzc.add('tzid', 'Europe/Berlin')
-    tzc.add('x-lic-location', 'Europe/Berlin')
-
-    tzs = icalendar.TimezoneStandard()
-    tzs.add('tzname', 'CET')
-    tzs.add('dtstart', datetime.datetime(1970, 10, 25, 3, 0, 0))
-    tzs.add('rrule', {'freq': 'yearly', 'bymonth': 10, 'byday': '-1su'})
-    tzs.add('TZOFFSETFROM', datetime.timedelta(hours=2))
-    tzs.add('TZOFFSETTO', datetime.timedelta(hours=1))
-
-    tzd = icalendar.TimezoneDaylight()
-    tzd.add('tzname', 'CEST')
-    tzd.add('dtstart', datetime.datetime(1970, 3, 29, 2, 0, 0))
-    tzs.add('rrule', {'freq': 'yearly', 'bymonth': 3, 'byday': '-1su'})
-    tzd.add('TZOFFSETFROM', datetime.timedelta(hours=1))
-    tzd.add('TZOFFSETTO', datetime.timedelta(hours=2))
-
-    tzc.add_component(tzs)
-    tzc.add_component(tzd)
-    calendar.add_component(tzc)
-
     berlin_timezone = pytz.timezone('Europe/Berlin')
 
     dtstamp = berlin_timezone.localize(now)
+
+    calendar = _calendar_header()
 
     for this_event in ics_data:
 
@@ -107,6 +119,11 @@ def merge_data(web_dir, json_dir):
             event.add('location', this_event['venue']['address'])
 
         calendar.add_component(event)
+
+        this_events_calendar = _calendar_header()
+        this_events_calendar.add_component(event)
+        with open(web_dir + 'ics/' + str(this_event['eventnumber']) + '.ics', 'wb') as output_file:
+            output_file.write(this_events_calendar.to_ical())
 
     with open(web_dir + 'techisb.ics', 'wb') as output_file:
         output_file.write(calendar.to_ical())
