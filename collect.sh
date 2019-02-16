2303146a4a76abf66b5b14878daa10f2fca9cff1
Dirk Gomez <dirk@dirkgomez.de>
2019-02-16 19:04:14 +0100

    Rename collect.sh to do_update.sh

--- collect.sh
+++
@@ -1,5 +0,0 @@
-for COLLECTOR in meetup curated eventbrite inberlin; do
-  (cd collector-${COLLECTOR} && python3 retrieve-${COLLECTOR}.py > ../data/${COLLECTOR}.json)
-done
-
-(cd merger && python3 merge.py "inberlin")
--- do_update.sh
+++ do_update.sh
@@ -1,20 +1,5 @@
-#!/bin/sh
-
-# collect events from all source streams
-for i in collector-*; do
-    echo "Collect from $i ..."
-    (cd $i && make build run)
+for COLLECTOR in meetup curated eventbrite inberlin; do
+  (cd collector-${COLLECTOR} && python3 retrieve-${COLLECTOR}.py > ../data/${COLLECTOR}.json)
 done
 
-# merge them together
-echo "Merging events ..."
-(cd merger && make build run)
-
-# start nginx if not running
-if netstat -lnt | grep -q :80; then
-    echo "Server is already running. Skipping."
-else
-    echo "Starting webserver ..."
-    (cd delivery && make run)
-fi
-echo "Done."
+(cd merger && python3 merge.py "inberlin")
